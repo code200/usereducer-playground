f24680cbb1d9785fe4d5fe6fd5672dc128368cd4
Nathan K <code200tech@gmail.com>
2023-06-05 22:28:32 -0700

    Change from useState to useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,31 +1,63 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const ACTION_TYPES = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement',
+  ADD_NUMBER: 'add_number',
+  SET_NUMBER_TO_ADD: 'set_number_to_add',
+};
+
+const reducer = (state, action) => {
+  console.log('reducer current state', state);
+  switch (action.type) {
+    case ACTION_TYPES.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case ACTION_TYPES.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case ACTION_TYPES.ADD_NUMBER:
+      return { ...state, count: state.count + action.payload };
+    case ACTION_TYPES.SET_NUMBER_TO_ADD:
+      return { ...state, numberToAdd: action.payload };
+    default:
+      return state;
+  }
+
+  // whatever we return from the reducer becomes the new state.
+};
+
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  const [numberToAdd, setNumberToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    numberToAdd: 0,
+  });
 
   const increment = () => {
-    setCount(count + 1);
+    dispatch({ type: ACTION_TYPES.INCREMENT });
   };
+
   const decrement = () => {
-    setCount(count - 1);
+    dispatch({ type: ACTION_TYPES.DECREMENT });
   };
   const handleFormSubmit = event => {
     event.preventDefault();
     // const formData = new FormData(event.target);
     // const number = formData.get('number');
 
-    setCount(count + Number(numberToAdd));
-    setNumberToAdd(0);
+    dispatch({ type: ACTION_TYPES.ADD_NUMBER, payload: state.numberToAdd });
+    dispatch({ type: ACTION_TYPES.SET_NUMBER_TO_ADD, payload: 0 });
   };
   const handleNumberChange = event => {
-    setNumberToAdd(+event.target.value); // if user hits delete key, NaN will be the value.
+    dispatch({
+      type: ACTION_TYPES.SET_NUMBER_TO_ADD,
+      payload: +event.target.value,
+    });
   };
+
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is {count}</h1>
+      <h1 className="text-lg">Count is {state.count}</h1>
       <div className="flex flex-row">
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
@@ -34,7 +66,7 @@ function CounterPage({ initialCount }) {
       <form onSubmit={handleFormSubmit}>
         <label>Add this number:</label>
         <input
-          value={numberToAdd || ''}
+          value={state.numberToAdd || ''}
           onChange={handleNumberChange}
           name="number"
           type="number"
